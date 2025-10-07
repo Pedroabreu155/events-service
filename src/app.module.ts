93828eeef7ce12d05002685f7d9c50f23e7127b1
Pedro <pedroliveirabreu@gmail.com>
2025-10-07 12:06:09 -0300

    feat: env module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,19 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+
 import { PrismaService } from './v1/prisma/prisma.service'
 import { PostEventController } from './v1/controllers/post-event.controller'
+import { envSchema } from './env/env'
+import { EnvModule } from './env/env.module'
 
 @Module({
+  imports: [
+    ConfigModule.forRoot({
+      validate: (env) => envSchema.parse(env),
+      isGlobal: true,
+    }),
+    EnvModule,
+  ],
   controllers: [PostEventController],
   providers: [PrismaService],
 })
---
+++ src/env/env.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
+import { z } from 'zod'
+
+export const envSchema = z.object({
+  DATABASE_URL: z.url(),
+  PORT: z.coerce.number().optional().default(3333),
+})
+
+export type Env = z.infer<typeof envSchema>
+
+@Injectable()
+export class EnvService {
+  constructor(private configService: ConfigService<Env, true>) {}
+
+  get<T extends keyof Env>(key: T) {
+    return this.configService.get<T>(key, { infer: true })
+  }
+}
