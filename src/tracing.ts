7df1e7d77af545d4db869d876625c64a839b51b9
Pedro <pedroliveirabreu@gmail.com>
2025-10-10 17:46:12 -0300

    jagger

--- src/tracing.ts
+++ src/tracing.ts
@@ -1,37 +1,24 @@
-import 'dotenv/config'
-
 import { NodeSDK } from '@opentelemetry/sdk-node'
 import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node'
-import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http'
-import { PrometheusExporter } from '@opentelemetry/exporter-prometheus'
+import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-grpc'
 
 export async function setupTracing() {
-  const collectorUrl = process.env.OTEL_COLLECTOR_URL
+  const url = process.env.OTEL_COLLECTOR_URL
 
-  // Trace exporter
   const traceExporter = new OTLPTraceExporter({
-    url: `${collectorUrl}/v1/traces`,
+    url,
   })
 
-  const prometheusExporter = new PrometheusExporter(
-    { port: 9464, host: '0.0.0.0', startServer: true },
-    () => {
-      console.log('Prometheus scrape endpoint: http://localhost:9464/metrics')
-    },
-  )
-
   const sdk = new NodeSDK({
-    instrumentations: [getNodeAutoInstrumentations()],
     traceExporter,
-    metricReaders: prometheusExporter,
+    instrumentations: [getNodeAutoInstrumentations()],
     serviceName: 'audit-service',
   })
 
-  await sdk.start()
-  console.log('[Tracing] OpenTelemetry started for service "audit-service"')
+  sdk.start()
+  console.log(`[Tracing] OpenTelemetry started for service`)
 
   process.on('beforeExit', async () => {
     await sdk.shutdown()
-    console.log('[Tracing] OpenTelemetry SDK shutdown')
   })
 }
